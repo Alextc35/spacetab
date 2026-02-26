44450b32e401de236b17bcc107e09dd29007f1aa
Alejandro Téllez <119206974+Alextc35@users.noreply.github.com>
2026-02-26 23:58:10 +0100

    refactor(i18n): simplify loadTranslations error guard

--- js/lang/index.js
+++ js/lang/index.js
@@ -8,9 +8,7 @@ export async function loadTranslations(lang) {
   const url = chrome.runtime.getURL(`js/lang/${lang}.json`);
 
   const response = await fetch(url);
-  if (!response.ok) {
-    throw new Error(`Failed to load translations: ${lang}`);
-  }
+  if (!response.ok) throw new Error(`Failed to load translations: ${lang}`);
 
   return response.json();
 }
